776b1ac084d3396630f1396c1805c872cc823be0
HPhuoc0906 <120120929+HPhuoc0906@users.noreply.github.com>
2023-04-22 23:17:56 +0700

    done

---
+++ INTRODUCTION/CHALLENGES/Network_Attacks.py
@@ -0,0 +1,10 @@
+def json_send(hsh):
+request = json.dumps(hsh).encode() tn.write(request)
+print(readline())
+print(readline())
+print(readline())
+print(readline())
+request = {
+}
+"buy": "flag"
+json_send(request)
