7cd0af7085c45c953f614900d01f42dfc71d9612
NgKhang3012 <127125359+NgKhang3012@users.noreply.github.com>
2023-04-23 22:24:58 +0700

    Update Favourite_byte.py

--- GENERAL/XOR/Favourite_byte.py
+++ GENERAL/XOR/Favourite_byte.py
@@ -1,15 +1,10 @@
-encrypted_msg = "0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104"
-encrypted_msg = bytes.fromhex(encrypted_msg)
-flag_format = b"crypto{"
-key = [O1 ^ O2
-for (O1, O2) in zip(encrypted_msg, flag_format)] + [ord("y")]
-flag = []
-key_len = len(key)
-for i in range(len(encrypted_msg)):
-    flag.append(
-    encrypted_msg[i] ^ key[i % key_len])
-flag = "".join(chr(o) for o in flag)
-
-
-print("Flag:")
-print (flag)
+string = "73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d"
+string_ord = [o for o in bytes.fromhex(string)]
+for order in range(256):
+   possible_flag_ord = [order^ o for o in string_ord]
+   possible_flag = "".join(chr(o) for o in possible_flag_ord)
+   if possible_flag.startswith("crypto"):
+      flag = possible_flag
+      break
+print("Flag: ")
+print(flag)
