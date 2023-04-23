49164be5a306f241113a30b5f5cf0a38ee017c5d
HPhuoc0906 <120120929+HPhuoc0906@users.noreply.github.com>
2023-04-23 16:46:45 +0700

    Create Bytes_and_Big_Integers.py

---
+++ GENERAL/ENCODING/Bytes_and_Big_Integers.py
@@ -0,0 +1,6 @@
+import base64
+print(base64.b64encode(bytes.fromhex( '72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf')))
+from Crypto.Util.number import *
+a = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
+C =   long_to_bytes(a)
+print(c)
